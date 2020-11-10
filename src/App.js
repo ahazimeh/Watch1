57bbe446b3326689b6e198bd11d2c0ceecdad581
ahazimeh <hazimeh95@gmail.com>
2020-11-11 01:10:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
+
+callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +34,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <p className="App-intro">{this.state.apiResponse}</p>
     </div>
   );
+}
 }
 
 export default App;
